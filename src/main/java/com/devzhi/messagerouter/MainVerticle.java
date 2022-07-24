9dc98def32789a1f837cabd7f9c2477fe33545a5
devzhi <devzhi@outlook.com>
2022-07-24 14:29:08 +0800

    部署连接和和路由模块

--- src/main/java/com/devzhi/messagerouter/MainVerticle.java
+++ src/main/java/com/devzhi/messagerouter/MainVerticle.java
@@ -1,23 +1,31 @@
 package com.devzhi.messagerouter;
 
+import com.devzhi.messagerouter.connection.ConnectVerticle;
+import com.devzhi.messagerouter.route.RouteVerticle;
+import io.vertx.config.ConfigRetriever;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Promise;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class MainVerticle extends AbstractVerticle {
 
-  @Override
-  public void start(Promise<Void> startPromise) throws Exception {
-    vertx.createHttpServer().requestHandler(req -> {
-      req.response()
-        .putHeader("content-type", "text/plain")
-        .end("Hello from Vert.x!");
-    }).listen(8888, http -> {
-      if (http.succeeded()) {
-        startPromise.complete();
-        System.out.println("HTTP server started on port 8888");
-      } else {
-        startPromise.fail(http.cause());
-      }
-    });
-  }
+    @Override
+    public void start(Promise<Void> startPromise) throws Exception {
+        // 加载配置文件
+        ConfigRetriever retriever = ConfigRetriever.create(vertx);
+        // 部署Verticle
+        retriever.getConfig(json -> {
+            vertx.deployVerticle(new ConnectVerticle(),
+                new DeploymentOptions().setConfig(json.result().getJsonObject("connect")))
+                .onFailure(hander -> {
+                log.error("[模块部署]连接模块部署失败:({}",hander.getCause().toString());
+                });
+            vertx.deployVerticle(new RouteVerticle(),
+                new DeploymentOptions().setConfig(json.result().getJsonObject("route"))).onFailure(hander -> {
+                log.error("[模块部署]路由模块部署失败:(\n{}",hander.getCause().toString());
+            });;
+        });
+    }
 }
