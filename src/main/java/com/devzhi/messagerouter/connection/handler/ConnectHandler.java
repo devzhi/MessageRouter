1de0ee06fdd3e53d9dd7f220cd289ae4dd6ece77
devzhi <devzhi@outlook.com>
2022-07-24 14:28:39 +0800

    提取name,type和eventBus等通用属性

--- src/main/java/com/devzhi/messagerouter/connection/handler/ConnectHandler.java
+++ src/main/java/com/devzhi/messagerouter/connection/handler/ConnectHandler.java
@@ -4,13 +4,31 @@ package com.devzhi.messagerouter.connection.handler;
 import com.devzhi.messagerouter.model.Message;
 import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.json.Json;
+import lombok.Data;
 
 /**
  * 连接服务接口
  *
  * @author devzhi
  */
+@Data
 public abstract class ConnectHandler {
+
+    /**
+     * 连接名称
+     */
+    private String name;
+
+    /**
+     * 类型
+     */
+    private String type;
+
+    /**
+     * 消息总线
+     */
+    private EventBus eventBus;
+
     /**
      * 连接
      * @return 返回连接后的对象
