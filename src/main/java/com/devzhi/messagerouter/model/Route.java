532f746bdec90cb0821c178878cea501a0d8c6c0
devzhi <devzhi@outlook.com>
2022-07-24 14:44:29 +0800

    编写路由实体类

---
+++ src/main/java/com/devzhi/messagerouter/model/Route.java
@@ -0,0 +1,14 @@
+package com.devzhi.messagerouter.model;
+
+import lombok.Data;
+
+/**
+ * 路由
+ *
+ * @author devzhi
+ */
+@Data
+public class Route {
+    private String source;
+    private String target;
+}
